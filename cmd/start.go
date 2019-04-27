157a8de59ceb29db7a823c36f8105c3a7578e6d9
Joe Frizzell <joefrizzell@Joes-MBP-2.lan1>
2019-04-27 17:34:31 -0400

    Update Unlock to RUnlock

--- cmd/start.go
+++ cmd/start.go
@@ -7,7 +7,7 @@ import (
 type OneAndOnlyNumber struct {
 	num        int
 	generation int
-	numMutext  sync.RWMutex
+	numMutex   sync.RWMutex
 }
 
 const MembersToNotify = 2
@@ -19,22 +19,22 @@ func InitTheNumber(val int) *OneAndOnlyNumber {
 }
 
 func (n *OneAndOnlyNumber) SetValue(newVal int) {
-	n.numMutext.Lock()
-	defer n.numMutext.Unlock()
+	n.numMutex.Lock()
+	defer n.numMutex.Unlock()
 	n.num = newVal
 	n.generation = n.generation + 1
 }
 
 func (n *OneAndOnlyNumber) GetValue() (int, int) {
-	n.numMutext.RLock()
-	defer n.numMutext.Unlock()
+	n.numMutex.RLock()
+	defer n.numMutex.RUnlock()
 	return n.num, n.generation
 }
 
 func (n *OneAndOnlyNumber) NotifyValue(curVal int, curGeneration int) bool {
 	if curGeneration > n.generation {
-		n.numMutext.Lock()
-		defer n.numMutext.Unlock()
+		n.numMutex.Lock()
+		defer n.numMutex.Unlock()
 		n.generation = curGeneration
 		n.num = curVal
 		return true
